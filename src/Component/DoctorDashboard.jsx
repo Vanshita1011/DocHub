475a73d2c58d603633a7b2471f40173bbc2b1026
Vanshita1011 <vanshitapatel400@gmail.com>
2025-04-14 17:59:51 +0530

    calender chnge

--- src/Component/DoctorDashboard.jsx
+++ src/Component/DoctorDashboard.jsx
@@ -13,7 +13,6 @@ import "./DoctorDashboard.css";
 import { BeatLoader } from "react-spinners";
 import api from "../axiosInterceptor";
 import { useUser } from "../UserContext";
-import DoctorCalendar from "./DoctorCalendar";
 
 export default function DoctorDashboard() {
   const { logout } = useUser();
@@ -343,7 +342,6 @@ export default function DoctorDashboard() {
                 </div>
               </Col>
             </Row>
-            <DoctorCalendar />
           </Container>
         </>
       )}
